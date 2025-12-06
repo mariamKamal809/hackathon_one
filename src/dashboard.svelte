f0c42ecd287b7ea16b3fbafe7b629d44efc71fbb
mariamKamal809 <mariamkamal6145@gmail.com>
2025-12-06 12:17:24 +0300

    dashoard

--- src/dashboard.svelte
+++ src/dashboard.svelte
@@ -5,9 +5,9 @@
 
 <main>
 
-  <div class="flex flex-row gap-3 mt-30">
+  <div class="flex flex-row gap-10 mt-30">
 
-    <div class="porfile flex-1 flex flex-col justify-center items-center gap-10 bg-white h-90 p-10">
+    <div class="border border-gray-500 rounded-3xl flex-1 flex flex-col justify-center items-center gap-10 h-90 p-10 mt-19">
       <div class="flex-1 flex flex-col justify-center items-center">
         <img class="w-40 h-40 rounded-2xl object-cover overflow-hidden" src="/src/assets/young-bearded-man-with-striped-shirt_273609-5677.avif" alt="usreImage">
       </div>
@@ -21,67 +21,68 @@
     </div>
 
     
-    <!-- <div class="text-5xl text-white font-bold mb-7">History</div> -->
-    <div class="flex-3 flex justify-start items-center gap-5 w-250 flex-wrap ">
+    <div class="flex-3 flex-col">
+      <div class="text-5xl text-white font-bold mb-7">History</div>
+      <div class="flex flex justify-start items-center gap-5 w-250 flex-wrap ">
 
-      <div class="border border-gray-500 w-80 rounded-3xl overflow-hidden flex flex-col">
-        <img class="object-cover" src="/src/assets/t3qWG.png" alt="textimage">
-        <div class="p-3 flex items-center gap-5">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text-icon lucide-file-text bg-purple-600 p-1 rounded-md"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>
-          <div>
-            <p class="text-xl text-white font-bold pt-1">image 1</p>
-            <p class="text-l text-white">date: 5/12/2025</p>
+        <div class="border border-gray-500 w-80 rounded-3xl overflow-hidden flex flex-col">
+          <img class="object-cover" src="/src/assets/t3qWG.png" alt="textimage">
+          <div class="p-3 flex items-center gap-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text-icon lucide-file-text bg-purple-600 p-1 rounded-md"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>
+            <div>
+              <p class="text-xl text-white font-bold pt-1">image 1</p>
+              <p class="text-l text-white">date: 5/12/2025</p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="border border-gray-500 w-80 rounded-3xl overflow-hidden flex flex-col">
-        <img class="object-cover" src="/src/assets/t3qWG.png" alt="textimage">
-        <div class="p-3 flex items-center gap-5">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text-icon lucide-file-text bg-purple-600 p-1 rounded-md"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>
-          <div>
-            <p class="text-xl text-white font-bold pt-1">image 1</p>
-            <p class="text-l text-white">date: 5/12/2025</p>
+        <div class="border border-gray-500 w-80 rounded-3xl overflow-hidden flex flex-col">
+          <img class="object-cover" src="/src/assets/t3qWG.png" alt="textimage">
+          <div class="p-3 flex items-center gap-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text-icon lucide-file-text bg-purple-600 p-1 rounded-md"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>
+            <div>
+              <p class="text-xl text-white font-bold pt-1">image 1</p>
+              <p class="text-l text-white">date: 5/12/2025</p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="border border-gray-500 w-80 rounded-3xl overflow-hidden flex flex-col">
-        <img class="object-cover" src="/src/assets/t3qWG.png" alt="textimage">
-        <div class="p-3 flex items-center gap-5">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text-icon lucide-file-text bg-purple-600 p-1 rounded-md"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>
-          <div>
-            <p class="text-xl text-white font-bold pt-1">image 1</p>
-            <p class="text-l text-white">date: 5/12/2025</p>
+        <div class="border border-gray-500 w-80 rounded-3xl overflow-hidden flex flex-col">
+          <img class="object-cover" src="/src/assets/t3qWG.png" alt="textimage">
+          <div class="p-3 flex items-center gap-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text-icon lucide-file-text bg-purple-600 p-1 rounded-md"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>
+            <div>
+              <p class="text-xl text-white font-bold pt-1">image 1</p>
+              <p class="text-l text-white">date: 5/12/2025</p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="border border-gray-500 w-80 rounded-3xl overflow-hidden flex flex-col">
-        <img class="object-cover" src="/src/assets/t3qWG.png" alt="textimage">
-        <div class="p-3 flex items-center gap-5">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text-icon lucide-file-text bg-purple-600 p-1 rounded-md"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>
-          <div>
-            <p class="text-xl text-white font-bold pt-1">image 1</p>
-            <p class="text-l text-white">date: 5/12/2025</p>
+        <div class="border border-gray-500 w-80 rounded-3xl overflow-hidden flex flex-col">
+          <img class="object-cover" src="/src/assets/t3qWG.png" alt="textimage">
+          <div class="p-3 flex items-center gap-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text-icon lucide-file-text bg-purple-600 p-1 rounded-md"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>
+            <div>
+              <p class="text-xl text-white font-bold pt-1">image 1</p>
+              <p class="text-l text-white">date: 5/12/2025</p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="border border-gray-500 w-80 rounded-3xl overflow-hidden flex flex-col">
-        <img class="object-cover" src="/src/assets/t3qWG.png" alt="textimage">
-        <div class="p-3 flex items-center gap-5">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text-icon lucide-file-text bg-purple-600 p-1 rounded-md"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>
-          <div>
-            <p class="text-xl text-white font-bold pt-1">image 1</p>
-            <p class="text-l text-white">date: 5/12/2025</p>
+        <div class="border border-gray-500 w-80 rounded-3xl overflow-hidden flex flex-col">
+          <img class="object-cover" src="/src/assets/t3qWG.png" alt="textimage">
+          <div class="p-3 flex items-center gap-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text-icon lucide-file-text bg-purple-600 p-1 rounded-md"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>
+            <div>
+              <p class="text-xl text-white font-bold pt-1">image 1</p>
+              <p class="text-l text-white">date: 5/12/2025</p>
+            </div>
           </div>
         </div>
+
       </div>
-
-
-
     </div>
+
   </div>
 </main>
 
