c42b24236766706eaad3bf294dc4f3d7377a5bda
mariamKamal809 <mariamkamal6145@gmail.com>
2025-12-06 12:16:58 +0300

    update dashboared

--- src/dashboard.svelte
+++ src/dashboard.svelte
@@ -5,32 +5,24 @@
 
 <main>
 
-  <div class="flex flex-col justify-center items-center gap-3 mt-30">
-    <div class="porfile flex justify-center items-center bg-white w-250 p-10">
-      <div class="flex-1 flex flex-col justify-center items-center gap-5 w-120">
-        <img class="userimage w-40 h-40 rounded-full object-cover overflow-hidden" src="/src/assets/young-bearded-man-with-striped-shirt_273609-5677.avif" alt="usreImage">
-        <div class="flex flex-row justify-center items-center gap-5">
-          <!-- svelte-ignore a11y_consider_explicit_label -->
-          <button class="flex flex-col justify-center items-center bg-white p-3 rounded-xl">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-image-up-icon lucide-image-up"><path d="M10.3 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10l-3.1-3.1a2 2 0 0 0-2.814.014L6 21"/><path d="m14 19.5 3-3 3 3"/><path d="M17 22v-5.5"/><circle cx="9" cy="9" r="2"/></svg>
-          </button>
-          <!-- svelte-ignore a11y_consider_explicit_label -->
-          <button class="flex flex-col justify-center items-center bg-white p-3 rounded-xl">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-camera-icon lucide-camera"><path d="M13.997 4a2 2 0 0 1 1.76 1.05l.486.9A2 2 0 0 0 18.003 7H20a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h1.997a2 2 0 0 0 1.759-1.048l.489-.904A2 2 0 0 1 10.004 4z"/><circle cx="12" cy="13" r="3"/></svg>
-          </button>
-        </div>
+  <div class="flex flex-row gap-3 mt-30">
+
+    <div class="porfile flex-1 flex flex-col justify-center items-center gap-10 bg-white h-90 p-10">
+      <div class="flex-1 flex flex-col justify-center items-center">
+        <img class="w-40 h-40 rounded-2xl object-cover overflow-hidden" src="/src/assets/young-bearded-man-with-striped-shirt_273609-5677.avif" alt="usreImage">
       </div>
-      <div class="flex-3 px-10">
-        <div class="text-3xl text-white font-bold">Name: <span>Mustafa Kamal</span></div>
-        <div class="text-3xl text-white font-bold">Email: <span>user@gmail.com</span></div>
-        <div class="text-3xl text-white font-bold">created in: 2/12/2025</div>
+
+      <div class="flex-3">
+        <div class="text-xl text-white font-bold">Name: <span>Mustafa Kamal</span></div>
+        <div class="text-xl text-white font-bold">Email: <span>user@gmail.com</span></div>
+        <div class="text-xl text-white font-bold">created in: 2/12/2025</div>
       </div>
 
     </div>
 
-    <div class="text-5xl text-white font-bold mb-7">History</div>
-
-    <div class="flex justify-start items-center gap-5 w-250 flex-wrap ">
+    
+    <!-- <div class="text-5xl text-white font-bold mb-7">History</div> -->
+    <div class="flex-3 flex justify-start items-center gap-5 w-250 flex-wrap ">
 
       <div class="border border-gray-500 w-80 rounded-3xl overflow-hidden flex flex-col">
         <img class="object-cover" src="/src/assets/t3qWG.png" alt="textimage">
